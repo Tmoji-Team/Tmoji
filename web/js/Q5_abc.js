f913ce74caabd434cca156b67ecfbe461d54c9b2
vy538 <good5538@gmail.com>
2020-05-03 20:59:17 -0400

    Q5 update

--- web/js/Q5_abc.js
+++ web/js/Q5_abc.js
@@ -1,7 +1,25 @@
 var single = '<div class="col-sm-6"><div class="card Q5_card" ><h3 class="Q5_card-title card-body">A</h3><p class="Q5_card-content">😅</p></div></div>'
 var newContent = "";
-for (i = 0; i < 10; i++) {
-  	newContent += single;
+
+var data = [['t', '😂'], ['s', '🙏'], ['d', '🗣'], ['p', '😍'], ['x', '👉'], ['a', '😉'], ['b', '👏'], ['i', '♀'], ['o', '💙'], ['k', '🙌'], ['c', '👍'], ['w', '💪']]
+
+nextR = 3
+
+for (i = 0; i < data.length; i++) {
+	if(i%nextR==0){
+		struc = '<div class="row"><div class="col-sm-4 "><div class="card Q5_card" style="max-width: 12rem;"><h3 class="Q5_card-title card-body">'
+	}else{
+		struc = '<div class="col-sm-4"><div class="card Q5_card" style="max-width: 12rem;"><h3 class="Q5_card-title card-body">'
+	}
+	temp = data[i]
+  	struc += temp[0].toUpperCase()
+  	struc += '</h3><p class="Q5_card-content">'
+  	struc += temp[1]
+  	struc += '</p></div></div>'
+  	if(i%nextR==nextR-1){
+		struc += '</div>'
+	}
+  	newContent+= struc
 }
 
 $('#Q5-content').append(newContent);  
